1789421ee1ec2f2815f713dffadb6f79ad708236
DenGrachev59 <2717087@mail.ru>
2023-06-30 20:16:14 +0500

    написание утилит и main

--- main.py
+++ main.py
@@ -1,10 +1,62 @@
-from classes import HHApi, SJApi
+from scr.classes import JSONSaver, HHApi, SJApi
+from scr.utils import get_vacancies_by_salary, sort_from_minimum_salary, get_top_vacancies
+
+
+def main():
+    def user_interaction():
+        print("Здравствуйте.")
+        search_request = input("Введите ключевой запрос по вакансии: ")
+        salary_min = int(input("Укажите минимальную ЗП "))
+        top_n = int(input("Сколько вакансий вывести из топ выборки? Введите число: "))
+
+        # Создание экземпляров классов
+        hh_api = HHApi()
+        sj_api = SJApi()
+
+        # вакансии с сайтов
+        hh_vacancies = hh_api.get_vacancies(search_request)
+        sj_vacancies = sj_api.get_vacancies(search_request)
+
+        # Сохранение файл
+        json_saver_hh = JSONSaver(search_request, "hh")
+        json_saver_hh.add_vacancy(hh_vacancies)
+
+        json_saver_sj = JSONSaver(search_request, "sj")
+        json_saver_sj.add_vacancy(sj_vacancies)
+
+        # Отбор данных
+        vac_hh = json_saver_hh.select()
+        vac_sj = json_saver_sj.select()
+
+        # Сортировка по указанной зарплате
+        sort_vac_hh = get_vacancies_by_salary(vac_hh, salary_min)
+        sort_vac_sj = get_vacancies_by_salary(vac_sj, salary_min)
+
+        # Сортировка списка по убывающей
+        sort_vac_hh = sort_from_minimum_salary(sort_vac_hh, True)
+        sort_vac_sj = sort_from_minimum_salary(sort_vac_sj, True)
+
+        # Выборка N количетсва из топ вакансий
+        top_vac_hh = get_top_vacancies(sort_vac_hh, top_n)
+        top_vac_sj = get_top_vacancies(sort_vac_sj, top_n)
+
+        print("\n", "*" * 150, "\n")
+        print("\n", "          ВАКАНСИИ с hh.ru", "\n")
+        print("\n","*" * 150, "\n")
+        for vacancy in top_vac_hh:
+            print(vacancy)
+            print("\n", "-" * 20, "\n")
+
+        print("\n","*" * 150, "\n")
+        print("\n", "          ВАКАНСИИ с superjob.ru", "\n")
+        print("\n","*" * 150, "\n")
+        for vacancy in top_vac_sj:
+            print(vacancy)
+            print("\n", "-" * 20, "\n")
+
+    user_interaction()
 
 
 if __name__ == "__main__":
-    # user_interaction()
-    v_hh = HHApi()
-    v_sj = SJApi()
 
-    # print (v_sj.get_vacancies('Python'))
-    print(v_hh.get_vacancies("Python"))
+    main()
--- scr/classes.py
+++ scr/classes.py
@@ -1,49 +1,45 @@
-import json
-import os
 from abc import ABC, abstractmethod
-
 import json
-
 import requests
 
+""" Постоянные переменные """
 API_KEY_SJ: str = "v3.r.137641108.6cde3c0a45d757a8dda83c208eac505a7e929aee.1ffce3b1beb8d72068c1117e275f2b36d532d224"
-VACANCIES: int = 3
-PER_PAGES: int = 2
+VACANCIES: int = 200
+PER_PAGES: int = 10
 API_URL_HH = "https://api.hh.ru/vacancies"
-EUR: float = 90.5
-USD: float = 85.3
+API_URL_SJ ="https://api.superjob.ru/2.0/vacancies/"
+EUR = 90
+USD = 85
 
 
 class AbstractClassApi(ABC):
     """
     Абстрактный класс для создания классов по работе с API
-
     """
 
     @abstractmethod
     def get_request(self):
         """
-        Метод для получения запроса от API
+        Получение запроса от API
         """
         pass
 
     @abstractmethod
     def get_vacancies(self):
+        """
+        Получение вакансий
+        """
         pass
 
 
 class HHApi(AbstractClassApi):
     """
      Класс для работы по API c сайтом www.hh.ru
-
     """
-
-    # api_url = "https://api.hh.ru/vacancies"
 
     def get_request(self, k_word, pages=10, api_url=API_URL_HH):
         """
-        Метод класса HHApi, который делает запрос на сайт hh.ru  по API (результат в JSON формате)
-
+        Метод класса HHApi, который делает запрос на сайт hh.ru по API (результат в JSON формате)
         """
         params = {'text': k_word, 'page': pages, 'per_page': PER_PAGES}
 
@@ -53,51 +49,41 @@ class HHApi(AbstractClassApi):
             print(f'ATTENTION!!!!! ***connection error during request*** \n {errors}\n')
 
     def get_vacancies(self, k_word):
-        """
-        Метод получения списка вакансий по ключевому слову
-
-        """
 
         list_vacancies = []
         for item in range(PER_PAGES):
             data = self.get_request(k_word, item)
-            list_vacancies.append(data)
+            list_vacancies.extend(data)
         return list_vacancies
 
 
 class SJApi(AbstractClassApi):
     """
-    Класс для работы по API c сайтом www.superjob.ru
-
+    Класс для работы по API c сайтом superjob.ru
     """
 
-    def get_request(self, k_word, pages):
+    def get_request(self, k_word, pages, api_url=API_URL_SJ):
         authorization = {'X-Api-App-Id': API_KEY_SJ}
         params = {'keyword': k_word, 'page': pages, 'count': VACANCIES}
 
         try:
-            return requests.get('https://api.superjob.ru/2.0/vacancies/', headers=authorization, params=params).json()[
+            return requests.get(api_url, headers=authorization, params=params).json()[
                 "objects"]
         except requests.exceptions.ConnectionError as errors:
             print(f'ATTENTION!!!!! ***connection error during request*** \n {errors}\n')
 
     def get_vacancies(self, k_word='Python developer'):
-        """
-        Метод получения списка вакансий по ключевому слову
-
-        """
 
         list_vacancies = []
         for item in range(PER_PAGES):
             data = self.get_request(k_word, item)
-            list_vacancies.append(data)
+            list_vacancies.extend(data)
         return list_vacancies
 
 
 class Vacancy:
     """
     Клас вакансии
-
     """
 
     def __init__(self, title, salary_from, salary_to, salary_currency, url, employer_name, address):
@@ -105,49 +91,47 @@ class Vacancy:
         self.__url = url
         self.__employer_name = employer_name
         self.__address = address
-        self.__salary_from = salary_from
-        self.__salary_to = salary_to
-        self.__salary_currency = salary_currency
+        self.salary_from = salary_from
+        self.salary_to = salary_to
+        self.salary_currency = salary_currency
 
     def __str__(self):
-        if self.__salary_currency:
-            self.__salary_currency = f"Вилка заработной платы({self.__salary_currency}) "
+        if self.salary_currency:
+            self.salary_currency = f"Вилка заработной платы ({self.salary_currency}) "
         else:
-            self.__salary_currency = "Уровень заработной платы не указан"
-        if self.__salary_from:
-            self.__salary_from = self.__salary_from
+            self.salary_currency = "Уровень заработной платы не указан"
+        if self.salary_from:
+            self.salary_from = self.salary_from
         else:
-            self.__salary_from = " нет данных "
-        if self.__salary_to:
-            self.__salary_to = self.__salary_to
+            self.salary_from = ""
+        if self.salary_to:
+            self.salary_to = self.salary_to
         else:
-            self.__salary_to = " нет данных "
+            self.salary_to = ""
         if not self.__address:
             self.__address = "Адрес не указан"
 
-        rez = f"Компания работодатель: {self.__employer_name}  / Вакансия:  {self.__title}\n" \
-              f"Ссылка на страницу вакансии: {self.__url} \n" \
-              f"{self.__salary_currency} от: {self.__salary_from} до: {self.__salary_to}\n" \
-              f"Адрес работодателя: {self.__address}"
+        rez = f"  Компания работодатель: {self.__employer_name} \n  Вакансия:  {self.__title}\n" \
+              f"  Ссылка на страницу вакансии: {self.__url} \n" \
+              f"  {self.salary_currency} от: {self.salary_from} до: {self.salary_to}\n" \
+              f"  Адрес работодателя: {self.__address}"
         return rez
 
     def __gt__(self, other):
-
         """
         Метод сравнения по минимальной зарплате
-
         """
-        if not other.__salary_from:
+
+        if not other.salary_from:
             return True
-        if not self.__salary_from:
+        if not self.salary_from:
             return False
-        return self.__salary_from >= other.__salary_from
+        return self.salary_from >= other.salary_from
 
 
 class AbstractClassJCONSaver(ABC):
     """
     Абстрактный класс для классов, по обработке данных о вакансиях
-
     """
 
     @abstractmethod
@@ -173,8 +157,8 @@ class JSONSaver(AbstractClassJCONSaver):
 
     def add_vacancy(self, data):
         """Добавляет вакансии в файл 'self.__file_name'"""
-        with open(self.__file_name, 'w', encoding="UTF-8") as file:
-            json.dump(data, file, indent=4, ensure_ascii=False)
+        with open(self.__file_name, 'w', encoding="UTF-8") as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
 
     def select(self):
         """Создаем экземпляры с нужными атрибутами. Переводим, если нужно, в рубли """
@@ -190,14 +174,18 @@ class JSONSaver(AbstractClassJCONSaver):
         if "hh" in self.filename:
             for row in data:
                 if row["salary"] and row["salary"]["to"] and row["salary"]["from"]:
-                    salary_from, salary_to, salary_currency = row["salary"]["from"], row["salary"]["to"], row["salary"][
-                        "currency"]
+                    salary_from = row["salary"]["from"]
+                    salary_to = row["salary"]["to"]
+                    salary_currency = row["salary"]["currency"]
+
                     if row["salary"]["currency"].upper() == "EUR":
-                        salary_from = row["salary"]["from"] *EUR
+                        salary_from = row["salary"]["from"] * EUR
                         salary_to = row["salary"]["to"] * EUR
+
                     if row["salary"]["currency"].upper() == "USD":
                         salary_from = row["salary"]["from"] * USD
                         salary_to = row["salary"]["to"] * USD
+
                     salary_currency = "RUB"
 
                 list_vac.append(Vacancy(
